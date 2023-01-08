b2149f3121ece736bfe5f50f1ffc08eed2bf9743
Glauber Bandeira <glauberbandeira_@hotmail.com>
2023-01-08 22:25:13 +1100

    first project commit

---
+++ app.py
@@ -0,0 +1,159 @@
+from flask import Flask, jsonify, request
+from estrutura_banco_de_dados import Autor, Postagem, app, db
+
+
+
+# Rota padrão - GET https://localhost:5000
+@app.route('/')
+def obter_postagens():
+    postagens = Postagem.query.all()
+
+    list_postagens = []
+    for postagem in postagens:
+        postagem_atual = {}
+        postagem_atual['titulo'] = postagem.titulo
+        postagem_atual['id_autor'] = postagem.id_autor
+        list_postagens.append(postagem_atual)
+    return jsonify({'postagens': list_postagens})
+
+# Obter postagem por id - GET https://localhost:5000/postagem/1
+
+
+@app.route('/postagem/<int:id_postagem>', methods=['GET'])
+def obter_postagem_por_indice(id_postagem):
+    postagem = Postagem.query.filter_by(id_postagem=id_postagem).first()
+    postagem_atual = {}
+    try:
+        postagem_atual['titulo'] = postagem.titulo
+    except:
+        pass
+    postagem_atual['id_autor'] = postagem.id_autor
+
+    return jsonify({'postagens': postagem_atual})
+
+# Criar uma nova postagem - POST https://localhost:5000/postagem
+
+
+@app.route('/postagem', methods=['POST'])
+def nova_postagem():
+    nova_postagem = request.get_json()
+    postagem = Postagem(
+        titulo=nova_postagem['titulo'], id_autor=nova_postagem['id_autor'])
+
+    db.session.add(postagem)
+    db.session.commit()
+
+    return jsonify({'mensagem': 'Postagem criada com sucesso'})
+
+# Alterar uma postagem existente - PUT https://localhost:5000/postagem/1
+
+
+@app.route('/postagem/<int:id_postagem>', methods=['PUT'])
+def alterar_postagem(id_postagem):
+    postagem_alterada = request.get_json()
+    postagem = Postagem.query.filter_by(id_postagem=id_postagem).first()
+    try:
+        postagem.titulo = postagem_alterada['titulo']
+    except:
+        pass
+    try:
+        postagem.id_autor = postagem_alterada['id_autor']
+    except:
+        pass
+
+    db.session.commit()
+    return jsonify({'mensagem': 'Postagem alterada com sucessso'})
+
+# Excluir uma postagem - DELETE - https://localhost:5000/postagem/1
+
+
+@app.route('/postagem/<int:id_postagem>', methods=['DELETE'])
+def excluir_postagem(id_postagem):
+    postagem_a_ser_excluida = Postagem.query.filter_by(
+        id_postagem=id_postagem).first()
+    if not postagem_a_ser_excluida:
+        return jsonify({'mensagem': 'Não foi encontrado uma postagem com este id'})
+    db.session.delete(postagem_a_ser_excluida)
+    db.session.commit()
+
+    return jsonify({'mensagem': 'Postagem excluída com sucesso!'})
+
+
+# API autores
+@app.route('/autores')
+def obter_autores():
+    autores = Autor.query.all()
+    lista_de_autores = []
+    for autor in autores:
+        autor_atual = {}
+        autor_atual['id_autor'] = autor.id_autor
+        autor_atual['nome'] = autor.nome
+        autor_atual['email'] = autor.email
+        lista_de_autores.append(autor_atual)
+
+    return jsonify({'autores': lista_de_autores})
+    
+
+
+@app.route('/autores/<int:id_autor>', methods=['GET'])
+def obter_autor_por_id(id_autor):
+    autor = Autor.query.filter_by(id_autor=id_autor).first()
+    if not autor:
+        return jsonify(f'Autor não encontrado')
+    autor_atual = {}
+    autor_atual['id_autor'] = autor.id_autor
+    autor_atual['nome'] = autor.nome
+    autor_atual['email'] = autor.email
+
+    return jsonify(f'Você buscou pelo autor: {autor_atual}')
+
+
+@app.route('/autores', methods=['POST'])
+def novo_autor():
+    print('deu erro')
+    novo_autor = request.get_json()
+    autor = Autor(
+        nome=novo_autor['nome'], senha=novo_autor['senha'], email=novo_autor['email']
+    )
+    db.session.add(autor)
+    db.session.commit()
+
+    return jsonify({'mensagem': 'Usuario criado com sucessso'}, 200)
+
+
+@app.route('/autores/<int:id_autor>', methods=['PUT'])
+def alterar_autor(id_autor):
+    usuario_a_alterar = request.get_json()
+    autor = Autor.query.filter_by(id_autor=id_autor).first()
+    if not autor:
+        return jsonify({'Mensagem': 'Este usuario não foi encontrado'})
+    try:
+            autor.nome = usuario_a_alterar['nome']
+    except:
+        pass
+    try:
+            autor.email = usuario_a_alterar['email']
+    except:
+        pass
+    try:
+            autor.senha = usuario_a_alterar['senha']
+    except:
+        pass
+
+    db.session.commit()
+    return jsonify({'mesagem': 'Usuario alterado com sucesso'})
+
+
+@app.route('/autores/<int:id_autor>', methods=['DELETE'])
+def excluir_autor(id_autor):
+    autor_existente = Autor.query.filter_by(id_autor=id_autor).first()
+    if not autor_existente:
+        return jsonify({'mensagem': 'O autor não foi encontrado!'})
+    db.session.delete(autor_existente)
+    db.session.commit()
+
+    return jsonify({'mensagem': 'O autor foi excluído com sucesso!'})
+
+
+if __name__ == '__main__':
+    app.run(port=5000, host='localhost', debug=True)
---
+++ estrutura_banco_de_dados.py
@@ -0,0 +1,48 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# Criar uma api flask
+app = Flask(__name__)
+
+# Criar uma instancia de SQLAlchemy
+app.config['SECRET_KEY'] = '123456'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+
+db = SQLAlchemy(app)
+db:SQLAlchemy
+
+# Definir a estrutura da tabela Postagem - id_postagem, titulo, autor
+class Postagem(db.Model):
+    # Estrutura
+    __tablename__ = 'postagem'
+    id_postagem = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String)
+    # Relacionamento da tabela postagem com a do autor
+    id_autor = db.Column(db.Integer,db.ForeignKey('autor.id_autor'))
+
+# Definir a estrutura da tabela Autor - id_autor, nome, email, senha, admin, postagens
+class Autor(db.Model):
+    __tablename__ = 'autor'
+    id_autor = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String)
+    email = db.Column(db.String)
+    senha = db.Column(db.String)
+    admin = db.Column(db.Boolean)
+    # relacionamento da tabela autor com a postagem
+    postagens = db.relationship('Postagem')
+
+def inicializar_banco():
+    with app.app_context():
+        # Executar o comando para criar o banco de dados
+        # Apaga qualquer estrutura previa e rodar apenas uma vez
+        db.drop_all()
+        # criar a estrutura inicial
+        db.create_all()
+
+        # Criando usuarios administradores
+        autor = Autor(nome='glauber',email='glauber@gmail.com',senha='123456',admin=True)
+        db.session.add(autor)
+        db.session.commit()
+
+if __name__=="__main__":
+    inicializar_banco()
